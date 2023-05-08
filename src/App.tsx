0bd7a51bf516a639e6085f8a7daafc3bb5347aa1
nassorc <macrossan17@gmail.com>
2023-05-08 00:27:31 -0700

    Update auth and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import AuthForm from './shared/components/AuthForm/AuthForm'
-import './App.css'
+import AuthForm from "./components/AuthForm/AuthForm";
+import Header from "./components/Header/Header";
+import HeaderButton from "./components/Button/HeaderButton/HeaderButton";
+import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div className="">
+      <Header name="I am de header" />
       <AuthForm />
+      <HeaderButton onClick={() => console.log("clicked")}>
+        <span>Header Button</span>
+      </HeaderButton>
+      <div className="h-[1000px]"></div>
     </div>
-
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,68 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Dropdown, DropdownItem } from "../Dropdown";
+import Button from "../Button";
+import HeaderButton from "../Button/HeaderButton/HeaderButton";
+import pageLogo from "/header-logo-icon.png";
+import {
+  faXmark,
+  faMoon,
+  faBars,
+  faCartShopping,
+} from "@fortawesome/free-solid-svg-icons";
+import { faSun } from "@fortawesome/free-regular-svg-icons";
+import { useEffect, useRef, useState } from "react";
+interface HeaderType {
+  name: string;
+}
+export default function Header(params: HeaderType) {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const dropdownButtonRef = useRef(null);
+  useEffect(() => {
+    document.addEventListener("click", (e) => {
+      console.log(e.target.closest("button"));
+      console.log(dropdownButtonRef.current);
+      if (e.target.closest("button") != dropdownButtonRef.current) {
+        setShowDropdown(false);
+      }
+    });
+  }, []);
+
+  return (
+    <header className="sticky z-50 top-0 px-4 h-20 bg-white border-b-2 border-b-slate-700/40 ">
+      <nav className="mx-auto max-w-screen-xl h-full flex justify-between items-center">
+        <div className="flex items-center">
+          <div className="w-14 h-14">
+            <img src={pageLogo} />
+          </div>
+          <p className="text-xl">PokeStore</p>
+        </div>
+        <div className="flex [&>*]:mr-2 last:m-0">
+          <Button className="relative bg-white w-12 h-12 hover:outline outline-2 outline-blue-700/50 rounded-md">
+            <FontAwesomeIcon icon={faCartShopping} />
+          </Button>
+          <Button className="relative bg-red-200 w-12 h-12 hover:outline outline-2 outline-blue-700/50 rounded-md">
+            <FontAwesomeIcon icon={faSun} />
+          </Button>
+          <Button
+            ref={dropdownButtonRef}
+            className={`relative bg-slate-700 w-12 h-12 hover:outline ${showDropdown ? "outline" : ""
+              } outline-2 outline-blue-700/50 rounded-md`}
+            onClick={() => setShowDropdown(!showDropdown)}
+          >
+            {showDropdown ? (
+              <FontAwesomeIcon icon={faXmark} className="text-white" />
+            ) : (
+              <FontAwesomeIcon icon={faBars} className="text-white" />
+            )}
+            <Dropdown show={showDropdown}>
+              <DropdownItem
+                leftIcon={<FontAwesomeIcon icon={faCartShopping} />}
+                text="Logout"
+              />
+            </Dropdown>
+          </Button>
+        </div>
+      </nav>
+    </header>
+  );
+}
